7a0babf12f914a5673bf48a0117b291bc67b20c9
Viktor Mogilenets <mogilinez17@yandex.ru>
2024-02-25 16:33:02 +0300

    copy

---
+++ 20240219/2/main.py
@@ -0,0 +1,42 @@
+from logic import Map, Game
+from entity import Monster, Player
+import exeptions
+
+
+def main():
+    game = Game(Map(), Player()) 
+    
+    while comand := input():
+        comand = comand.split()
+        match comand:
+            case ['left'| 'right' | 'up' | 'down']:
+                res = game.go_to(comand[0])
+                print(f'Moved to ({res[0][0]}, {res[0][1]})')
+                if res[1]:
+                    print(res[1])
+
+            case ['addmon', x, y, hello]:
+
+                try:
+                    x, y = int(x), int(y)
+                except Exception:
+                    print('Invalid arguments')
+                    continue
+                
+                ans = f'Added monster to ({x}, {y}) saying {hello}'
+                try:
+                    game.addmon((x, y), hello)
+                except exeptions.ReplaseMonster:
+                    ans += '\nReplaced the old monster'
+                except exeptions.IncorectArgument:
+                    print('Invalid arguments')
+                    continue
+                finally:
+                    print(ans)
+
+            case _:
+                print('Invalid command')
+
+
+if __name__ == '__main__':
+    main()
